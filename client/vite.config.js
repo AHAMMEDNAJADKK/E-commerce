1eb1f54e80829ded3e204e42718f126eb831d4fe
AHAMMEDNAJADKK <najadahammed34@gmail.com>
2026-03-12 15:07:09 +0530

    fix vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(),
   tailwindcss(),
@@ -10,7 +10,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-        target: `${import.meta.env.VITE_API_URL}`,
+        target:"https://e-commerce-i90r.onrender.com",
         changeOrigin:true,
       },
     },
